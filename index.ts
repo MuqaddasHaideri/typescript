e7ebff4d17060e26bfe4206d8838eff6db9a6c2c
MuqaddasHaideri <Muqaddashkhan0702@gmail.com>
2024-02-18 20:58:46 +0500

    adding inital files

---
+++ index.ts
@@ -0,0 +1,13 @@
+type myFileType ={
+    name?:string;
+    id:number;
+    message:string;
+}
+
+
+const myFile:myFileType = {
+    id:1,
+    message:"string"
+}
+
+console.log("this is my file=======",myFile)
