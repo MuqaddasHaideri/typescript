6ad54c567ef35c5e7eeb7cad2949a1beb5b376f0
MuqaddasHaideri <muqaddashkhan0702@gmail.com>
2024-02-18 23:16:19 +0500

    Adding files

---
+++ question2.js
@@ -0,0 +1,9 @@
+"use strict";
+//question2 
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+Object.defineProperty(exports, "__esModule", { value: true });
+var username = "MUQADDAS Haideri";
+var lowercase = username.toLowerCase();
+console.log("conversion of name [ " + username + " ] into lowercase ===> " + lowercase);
+//  
+exports.default = {};
---
+++ question2.ts
@@ -0,0 +1,12 @@
+
+//question2 
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+
+let username = "MUQADDAS Haideri";
+
+let lowercase : string = username.toLowerCase();
+
+console.log("conversion of name [ "+username+" ] into lowercase ===> "+lowercase );
+
+//  
