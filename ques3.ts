7b8dca5a172c0c5c8a2daa0c61570d8b498d53b0
MuqaddasHaideri <muqaddashkhan0702@gmail.com>
2024-02-19 14:05:35 +0500

    name change

---
+++ ques3.ts
@@ -0,0 +1,20 @@
+
+//question2 
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let username = "Muqaddas haideri Zafar haider";
+
+let lowercase = username.toLowerCase();
+let uppercase= username.toUpperCase();
+function toTitleCase(username: string) {
+    return username.replace(/\b\w/g, function(username) {
+        return username.toUpperCase();
+    });
+}
+let titlecase = toTitleCase(username);
+console.log("conversion of name [ "+username+" ] into lowercase ===> "+lowercase );
+console.log("conversion of name [ "+username+" ] into uppercase ===> "+uppercase );
+console.log("conversion of name [ "+username+" ] into titlecase ===> "+titlecase );
+
+
+
