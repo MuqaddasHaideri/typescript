e23ff933c4ca73f84c27a6b4d185146c749fda0e
MuqaddasHaideri <muqaddashkhan0702@gmail.com>
2024-02-21 17:49:23 +0500

    save file

---
+++ ques6,7,8,9,10.ts
@@ -0,0 +1,39 @@
+//Name : Muqaddas haideri
+// Date : 19-feb-2024
+
+
+//question 6
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+console.log("question 6");
+
+let myname = "\tMuqaddas Haideri\t";
+let line = "Hello!\nMy name is\n"
+
+console.log(line+myname);
+
+//question 7,8
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log("question 7 and 8");
+
+
+//addition
+console.log("Addition : "+(4+4));
+
+//subtraction
+console.log("Subtraction : " +(12-4));
+
+//multiplication
+console.log("Multiplication : "+(2*4));
+
+//division
+console.log("Division :" +(16/2));
+
+//question 9
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+console.log("question 9");
+
+const number = 7;
+ console.log("My favorite number is "+number);
+
+
+ 
