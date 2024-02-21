d91129d5389f69f38599c390fe88b893116c4196
MuqaddasHaideri <muqaddashkhan0702@gmail.com>
2024-02-21 17:35:11 +0500

    save file

--- ques3.ts
+++ ques3.ts
@@ -1,3 +1,6 @@
+//Name : Muqaddas haideri
+// Date : 19-feb-2024
+
 
 //question2 
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
