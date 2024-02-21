6162455540a2ff53528e1de9115c0a6251f17e2b
MuqaddasHaideri <muqaddashkhan0702@gmail.com>
2024-02-21 17:34:04 +0500

    savefile

--- ques2.ts
+++ ques2.ts
@@ -1,6 +1,10 @@
+//Name : Muqaddas haideri
+// Date : 19-feb-2024
+
 
 //question 1:
 //Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+console.log("question 2");
 
 
 let userName = "Muqaddas";
