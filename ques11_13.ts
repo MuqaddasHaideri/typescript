a825f29ea601624a61f1486bce1fc0170c3376da
MuqaddasHaideri <muqaddashkhan0702@gmail.com>
2024-02-22 20:19:53 +0500

    save file"

---
+++ ques11_13.ts
@@ -0,0 +1,32 @@
+//Name : Muqaddas haideri
+// Date : 19-feb-2024
+
+
+//question 11
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+console.log("question 11");
+
+let names  = ["Wajiha","Alya","Ayesha","Naila"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+//question 12
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+console.log("question   12");
+
+console.log("I have many friends but a friend like you is hard to find >> "+names[0]);
+console.log("I am lucky to have such a good friend like u >> "+names[1]);
+console.log("Incase you forgot that, i wanna remind you are such an amazing friend >> "+names[2]); 
+console.log("It's been more than 10 years of our friendship >> "+names[3]);
+
+
+//question 13
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+console.log("question 13");
+
+let cars=["Corolla","Supra","Tesla"];
+console.log("My Dad had toyota [ " +cars[0]+ " ]");
+console.log("I saw a reel of a car named [ "+cars[1]+ " ]. it is a beautiful car");
+console.log ("I just remember the car name [ "+cars[2]+ "]. because of the famous scientist named Nikola Tesla, ");
