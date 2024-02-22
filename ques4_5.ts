9a5c0843b40f1f67e02a31b87933b8e2300c027d
MuqaddasHaideri <muqaddashkhan0702@gmail.com>
2024-02-22 20:18:28 +0500

    save file

---
+++ ques4_5.ts
@@ -0,0 +1,20 @@
+//Name : Muqaddas haideri
+// Date : 19-feb-2024
+
+//question 4
+
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+console.log("question 4");
+
+console.log("Brucelee once said \"Mistakes are always forgivable, if one has the courage to admit them.\" ");
+
+
+//question 5
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+console.log("question 5");
+
+let famous_person ="Imran khan";
+let message ="\"You only lose when u GIVEUP!\"";
+
+console.log(famous_person + " once said " +message);
